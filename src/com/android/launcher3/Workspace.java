SuperiorOS/android_packages_apps_Launcher3

Launcher3: Workspace: Allow resizing any widget
Change-Id: Iba122eb45d4ba81e53267ff341addfe281cf02fe
Signed-off-by: Joey Rizzoli <joey@lineageos.org>
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

src/com/android/launcher3/Workspace.java
@@ -1887,8 +1887,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
                         final LauncherAppWidgetHostView hostView = (LauncherAppWidgetHostView) cell;
                         AppWidgetProviderInfo pInfo = hostView.getAppWidgetInfo();
-                        if (pInfo != null && pInfo.resizeMode != AppWidgetProviderInfo.RESIZE_NONE
-                                && !d.accessibleDrag) {
+                        if (pInfo != null && !d.accessibleDrag) {
                             onCompleteRunnable = new Runnable() {
                                 public void run() {
                                     if (!isPageInTransition()) {

clhexdumpyard/packages_apps_launcher3-ancient

Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

src/com/android/launcher3/Workspace.java
@@ -1882,8 +1882,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
                         final LauncherAppWidgetHostView hostView = (LauncherAppWidgetHostView) cell;
                         AppWidgetProviderInfo pInfo = hostView.getAppWidgetInfo();
-                        if (pInfo != null && pInfo.resizeMode != AppWidgetProviderInfo.RESIZE_NONE
-                                && !options.isAccessibleDrag) {
+                        if (pInfo != null && !options.isAccessibleDrag) {
                             onCompleteRunnable = () -> {
                                 if (!isPageInTransition()) {
                                     AppWidgetResizeFrame.showForWidget(hostView, cellLayout);